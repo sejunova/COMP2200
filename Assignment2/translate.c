feba591abcb98c914b243c1a70fe87edf463baf4
sejun <sejun@quryon.com>
2019-10-13 16:04:25 +0900

    [ASS2]

--- Assignment2/translate.c
+++ Assignment2/translate.c
@@ -11,6 +11,8 @@ int translate(int argc, const char** argv)
     int i;
     int c;
     char temp_char;
+    char prev_char;
+    char next_char;
 
     char set1_translated[BUFFER_SIZE];
     char set2_translated[BUFFER_SIZE];
@@ -24,7 +26,6 @@ int translate(int argc, const char** argv)
     int set1_count = 0;
     int set2_count = 0;
     const char* last_range_end_index = NULL;
-	return 0;
 
     /* 인자 수가 안 맞는 경우 오류 반환 */
     if (!(argc == 3 || (argc == 4 && strcmp(argv[1], "-i") == 0))) {
@@ -48,6 +49,10 @@ int translate(int argc, const char** argv)
 
     /* SET1 PART*/
     while (*p_set1 != '\0') {
+        if (*p_set1 == 39) {
+            p_set1++;
+            continue;
+        }
         if (set1_count == BUFFER_SIZE) {
             return argument_too_long;
         }
@@ -83,14 +88,43 @@ int translate(int argc, const char** argv)
                 (last_range_end_index != NULL && p_set1 - 1 == last_range_end_index)) {
                 set1_translated[set1_count] = '-';
             } else {
-                if (*(p_set1 - 1) > *(p_set1 + 1)) {
+                if (*(p_set1 + 1) == 92) {
+                    p_set1++;
+                    if (*(p_set1 + 1) == 92) {
+                        next_char = '\\';
+                    } else if (*(p_set1 + 1) == 'a') {
+                        next_char = '\a';
+                    } else if (*(p_set1 + 1) == 'b') {
+                        next_char = '\b';
+                    } else if (*(p_set1 + 1) == 'f') {
+                        next_char = '\f';
+                    } else if (*(p_set1 + 1) == 'n') {
+                        next_char = '\n';
+                    } else if (*(p_set1 + 1) == 'r') {
+                        next_char = '\r';
+                    } else if (*(p_set1 + 1) == 't') {
+                        next_char = '\t';
+                    } else if (*(p_set1 + 1) == 'v') {
+                        next_char = '\v';
+                    } else if (*(p_set1 + 1) == 39) {
+                        next_char = '\'';
+                    } else if (*(p_set1 + 1) == 34) {
+                        next_char = '\"';
+                    } else {
+                        return invalid_format;
+                    }
+                } else {
+                    next_char = *(p_set1 + 1);
+                }
+                prev_char = set1_translated[set1_count - 1];
+                if (prev_char > next_char) {
                     return invalid_range;
                 }
-                for (i = 0; (char)(*(p_set1 - 1) + i) < *(p_set1 + 1); ++i) {
+                for (i = 0; (char)(set1_translated[set1_count - 1]) < next_char; ++i) {
                     if (set1_count == BUFFER_SIZE) {
                         return argument_too_long;
                     }
-                    set1_translated[set1_count] = (char)(*(p_set1 - 1) + i + 1);
+                    set1_translated[set1_count] = (char)(set1_translated[set1_count - 1] + 1);
                     set1_count++;
                 }
                 p_set1++;
@@ -108,8 +142,9 @@ int translate(int argc, const char** argv)
     last_range_end_index = NULL;
     /* SET2 PART*/
     while (*p_set2 != '\0') {
-        if (set2_count == set1_count) {
-            break;
+        if (*p_set2 == 39) {
+            p_set2++;
+            continue;
         }
         if (set2_count == BUFFER_SIZE) {
             return argument_too_long;
@@ -146,14 +181,43 @@ int translate(int argc, const char** argv)
                 (last_range_end_index != NULL && p_set2 - 1 == last_range_end_index)) {
                 set2_translated[set2_count] = '-';
             } else {
-                if (*(p_set2 - 1) > *(p_set2 + 1)) {
+                if (*(p_set2 + 1) == 92) {
+                    p_set2++;
+                    if (*(p_set2 + 1) == 92) {
+                        next_char = '\\';
+                    } else if (*(p_set2 + 1) == 'a') {
+                        next_char = '\a';
+                    } else if (*(p_set2 + 1) == 'b') {
+                        next_char = '\b';
+                    } else if (*(p_set2 + 1) == 'f') {
+                        next_char = '\f';
+                    } else if (*(p_set2 + 1) == 'n') {
+                        next_char = '\n';
+                    } else if (*(p_set2 + 1) == 'r') {
+                        next_char = '\r';
+                    } else if (*(p_set2 + 1) == 't') {
+                        next_char = '\t';
+                    } else if (*(p_set2 + 1) == 'v') {
+                        next_char = '\v';
+                    } else if (*(p_set2 + 1) == 39) {
+                        next_char = '\'';
+                    } else if (*(p_set2 + 1) == 34) {
+                        next_char = '\"';
+                    } else {
+                        return invalid_format;
+                    }
+                } else {
+                    next_char = *(p_set2 + 1);
+                }
+                prev_char = set2_translated[set2_count - 1];
+                if (prev_char > next_char) {
                     return invalid_range;
                 }
-                for (i = 0; (char)(*(p_set2 - 1) + i) < *(p_set2 + 1); ++i) {
+                for (i = 0; (char)(set2_translated[set2_count - 1]) < next_char; ++i) {
                     if (set2_count == BUFFER_SIZE) {
                         return argument_too_long;
                     }
-                    set2_translated[set2_count] = (char)(*(p_set2 - 1) + i + 1);
+                    set2_translated[set2_count] = (char)(set2_translated[set2_count - 1] + 1);
                     set2_count++;
                 }
                 p_set2++;
@@ -172,8 +236,10 @@ int translate(int argc, const char** argv)
             set2_translated[set2_count] = temp_char;
             set2_count++;
         }
+        set2_translated[set2_count] = '\0';
+    } else {
+        set2_translated[set1_count] = '\0';
     }
-    set2_translated[set2_count] = '\0';
 
     /* CHANGE CHAR FROM STDIN AND PRINT TO STDOUT */
     c = fgetc(stdin);

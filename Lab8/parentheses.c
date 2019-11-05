4e5814d3b9ab1b2ef3fa899a4ef9426f70348e80
sejun <sejun@quryon.com>
2019-11-05 19:49:01 +0900

    [lab8] third

--- Lab8/parentheses.c
+++ Lab8/parentheses.c
@@ -1,23 +1,20 @@
 #include "parentheses.h"
 #include <stdlib.h>
 
-size_t s_stack_count = 0;
-size_t s_stack_size = 2;
-
-void push(parenthesis_index_t** stack, char parenthesis, size_t index)
+void push(parenthesis_index_t** stack, char parenthesis, size_t index, size_t* stack_size_ptr, size_t* stack_count_ptr)
 {
-    if (s_stack_count == s_stack_size) {
-        s_stack_size *= 2;
-        *stack = realloc(*stack, s_stack_size * sizeof(parenthesis_index_t));
+    if (*stack_count_ptr == *stack_size_ptr) {
+        *stack_size_ptr *= 2;
+        *stack = realloc(*stack, *stack_size_ptr * sizeof(parenthesis_index_t));
     }
-    (*stack)[s_stack_count].parenthesis = parenthesis;
-    (*stack)[s_stack_count].index = index;
-    s_stack_count++;
+    (*stack)[*stack_count_ptr].parenthesis = parenthesis;
+    (*stack)[*stack_count_ptr].index = index;
+    (*stack_count_ptr)++;
 }
 
-parenthesis_index_t pop(parenthesis_index_t* stack)
+parenthesis_index_t pop(parenthesis_index_t* stack, size_t* stack_count_ptr)
 {
-    return stack[--s_stack_count];
+    return stack[--*stack_count_ptr];
 }
 
 int compare_stack(const void* a, const void* b)
@@ -29,17 +26,18 @@ int compare_stack(const void* a, const void* b)
 
 size_t get_matching_parentheses(parenthesis_t* parentheses, size_t max_size, const char* str)
 {
-    parenthesis_index_t* stack = (parenthesis_index_t*)malloc(sizeof(parenthesis_t) * s_stack_size);
+    size_t stack_size = 2;
+    size_t stack_count = 0;
+    parenthesis_index_t* stack = (parenthesis_index_t*)malloc(sizeof(parenthesis_t) * stack_size);
     size_t parentheses_count = 0;
     const char* p = str;
     size_t index = 0;
 
     while (*p != '\0') {
         if (*p == '{' || *p == '[' || *p == '(' || *p == '<') {
-            push(&stack, *p, index);
-        }
-        else if ((s_stack_count != 0) && ((*p == '}' && stack[s_stack_count - 1].parenthesis == '{') || (*p == ')' && stack[s_stack_count - 1].parenthesis == '(') || (*p == ']' && stack[s_stack_count - 1].parenthesis == '[') || (*p == '>' && stack[s_stack_count - 1].parenthesis == '<'))) {
-            parentheses[parentheses_count].opening_index = pop(stack).index;
+            push(&stack, *p, index, &stack_size, &stack_count);
+        } else if ((stack_count != 0) && ((*p == '}' && stack[stack_count - 1].parenthesis == '{') || (*p == ')' && stack[stack_count - 1].parenthesis == '(') || (*p == ']' && stack[stack_count - 1].parenthesis == '[') || (*p == '>' && stack[stack_count - 1].parenthesis == '<'))) {
+            parentheses[parentheses_count].opening_index = pop(stack, &stack_count).index;
             parentheses[parentheses_count].closing_index = index;
             parentheses_count++;
             if (parentheses_count == max_size) {
